7edc36df0fc8efee6fff42bd5735d02039d4d6e6
johannesstadler <stadler.joh@gmail.com>
2021-03-21 20:06:04 +0100

    fused the 2 projects

--- backend/backend/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path(r'test/', views.index, name='index'),
-]
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,14 @@
-import React, { Component } from 'react';
-import './App.css';
-
-const headers = new Headers({
-  Accept: "application/json",
-  "Content-Type": "application/json"
-});
+import React, { Component, Fragment } from "react";
+import Header from "./components/Header";
+import Home from "./components/Home";
 
 class App extends Component {
-  componentWillMount() {
-    this.testBackend()
-  }
-
-  state = {
-    test: ''
-  }
-
-  testBackend = async () => {
-    const test = await fetch("http://localhost/test/", {
-      method: "GET",
-      headers: headers,
-      cache: "default"
-    });
-    const testJson = await test.json();
-    if(testJson && testJson.status)
-      this.setState({ test: testJson.status });
-  }
-
   render() {
     return (
-      <div className="App">
-        <p className="App-intro">
-          From backend: {this.state.test}
-        </p>
-      </div>
+      <Fragment>
+        <Header />
+        <Home />
+      </Fragment>
     );
   }
 }
